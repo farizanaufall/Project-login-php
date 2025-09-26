fee274c23af5fb09321635cdebc67da254fe99f0
farizanaufall <farizanaufal71@gmail.com>
2025-09-26 16:43:06 +0700

    proses login

--- login.php
+++ login.php
@@ -1,4 +1,27 @@
+<?php
+session_start();
 
+if (isset($_SESSION['username'])) {
+    header("Location: dasboard.php");
+    exit;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+
+    if ($username === 'kipcair' && $password === '123') {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = 'Dosen';
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html>
